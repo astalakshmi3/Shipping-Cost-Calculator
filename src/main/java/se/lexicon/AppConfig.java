94ae0824d96267e71ebbf11488d47cbe86e76f63
Astalakshmi Muralidharan <astalakshmimuralidharan@gmail.com>
2026-02-11 12:05:38 +0100

    Refactor this application using Spring framework.

--- src/main/java/se/lexicon/AppConfig.java
+++ src/main/java/se/lexicon/AppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-    @ComponentScan (basePackages = "se.lexicon")
+    @ComponentScan
     @PropertySource("classpath:application.properties")
 public class AppConfig {
 }
