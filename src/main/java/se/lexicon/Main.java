02c279bebdd031b2f1231c41326263a25dd3d782
Astalakshmi Muralidharan <astalakshmimuralidharan@gmail.com>
2026-02-13 16:41:39 +0100

    Refactor this application using Spring framework.

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -17,9 +17,15 @@ public class Main {
 
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         ShippingService service = context.getBean (ShippingService.class);
-        ShippingRequest req = new ShippingRequest(Destination.DOMESTIC,Speed.STANDARD,5.0);
-        double cost = service.quote (req);
-        System.out.println("Shipping cost:" + cost);
+        System.setProperty("spring.profiles.active","dev");
+        ShippingRequest r1 = new ShippingRequest(Destination.DOMESTIC,Speed.STANDARD,5.0);
+        System.out.println("Domestic Standard :" + service.quote(r1));
+        ShippingRequest r2 = new ShippingRequest(Destination.INTERNATIONAL,Speed.EXPRESS, 20.0);
+        System.out.println("International Express :" + service.quote(r2));
+        ShippingRequest r3 = new ShippingRequest(Destination.DOMESTIC,Speed.EXPRESS, 10.0);
+        System.out.println("Domestic Express:" + service.quote(r3));
+        ShippingRequest r4 = new ShippingRequest (Destination.INTERNATIONAL,Speed.STANDARD,10.0 );
+        System.out.println("International Standard :" + service.quote(r4));
         context.close();
 
 
